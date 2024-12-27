95cbe2e2eaa0f4e0dfa61f6ed94694f6182b5222
Rakha Fausta <rakhafausta07@gmail.com>
2024-12-27 11:07:44 +0700

    add: pdf

--- 1. FUNCTION/Default Parameters/s.py
+++ 1. FUNCTION/Default Parameters/s.py
@@ -4,8 +4,15 @@
 # parameter c dan b memiliki default variable dan parameter a tidak.
 # namun apabila parameter a dan c memiliki default variable dan b tidak, maka akan terjadi error.
 
-def print_star(n = '1'):
+def print_star(n = 2):
   for i in range(n):
     print("*****************************")
 
-print_star(3)
+print_star(4)
+
+def add_numbers(a, b=0, c=0):
+    return a + b + c
+
+print(add_numbers(5))  # Output: 5
+print(add_numbers(5, 10))  # Output: 15
+print(add_numbers(5, 10, 15))  # Output: 30
--- 4. Closure/Closure Concept/s.py
+++ 4. Closure/Closure Concept/s.py
@@ -1,8 +1,8 @@
 def closure_calc():
-  a = 2
-  def mult(x):
-    return a * x
-  return mult
-  
-c = closure_calc()
-print(c(1),c(2),c(3))
+    a = 2
+    def mult(x):
+        return a * x
+    return mult
+
+c = closure_calc()  # Mengembalikan fungsi mult, tetapi belum dijalankan
+print(c(1))  # Baru di sini fungsi mult dijalankan, dengan x=1

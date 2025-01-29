afc62d201960e023160ff27d4ba9041d1c7083f3
Rakha Fausta <140504394+rakhaafd@users.noreply.github.com>
2025-01-29 21:57:16 +0700

    Delete project-gabut-py directory

--- project-gabut-py/find-factorial.py
+++
@@ -1,9 +0,0 @@
-userI = int(input('masukkan angka untuk difaktorial: '))
-def factorial (n):
-    if n <= 1:
-        return 1
-    else:
-        return n * factorial(n - 1)
-
-fac = factorial(userI)
-print(fac)

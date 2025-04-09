2031ee75b194b4365ec1e792ae82637b27a1e073
Rakha Fausta <140504394+rakhaafd@users.noreply.github.com>
2025-04-09 20:28:35 +0700

    Delete my_leveling/tes.py

--- my_leveling/tes.py
+++
@@ -1,8 +0,0 @@
-import pywhatkit as kit
-
-# Nomor tujuan (format internasional)
-nomor = "+62895339023888"
-pesan = "Halo, ini adalah pesan otomatis dari Python!"
-
-# Mengirim pesan langsung tanpa menunggu
-kit.sendwhatmsg_instantly(nomor, pesan, wait_time=10)  # Tunggu 10 detik sebelum mengirim
